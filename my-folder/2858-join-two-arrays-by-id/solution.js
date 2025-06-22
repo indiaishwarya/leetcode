c78a617dddf75711b8350de4dd7a1781b5c1822c
Aishwarya Indi <indiaishwarya97@gmail.com>
2025-06-22 19:14:05 +0000

    [LeetCode Sync] Runtime - 271 ms (76.54%), Memory - 99.1 MB (85.11%)

---
+++ my-folder/2858-join-two-arrays-by-id/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    let res = {};
+    let arr = [...arr1, ...arr2];
+    for(let obj of arr) {
+        let id = obj.id;
+        if(res[id]) {
+            res[id] = {...res[id], ...obj}
+        } else {
+            res[id] = obj
+        }
+    }
+    return Object.values(res);
+};
