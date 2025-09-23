3c20b0facbe348749d95e7d1d21cde2708db604d
Aishwarya Indi <indiaishwarya97@gmail.com>
2025-09-23 02:52:34 +0000

    [LeetCode Sync] Runtime - 57 ms (6.92%), Memory - 56 MB (6.26%)

--- my-folder/1894-merge-strings-alternately/solution.js
+++ my-folder/1894-merge-strings-alternately/solution.js
@@ -4,10 +4,18 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let lgth = word1.length > word2.length ? word1.length : word2.length;
-    let newWord = '';
-    for(let i = 0; i<lgth; i++) {
-        newWord += word1[i] + word2[i]
+    let newStr = "";
+    let addStr = word1 + word2;
+    let j=addStr.length - word2.length;
+    let strLength = word1.length < word2.length ? word2.length : word1.length
+    for(let i=0; i<strLength; i++) {
+        if(word1[i] === undefined)
+            newStr += word2[i];
+        else if(word2[i] === undefined)
+            newStr += word1[i];
+        else
+            newStr += word1[i] + word2[i];
     }
-    return newWord.replaceAll('undefined', '');
+    return newStr;
+    
 };
