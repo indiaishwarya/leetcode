dc8339be5cc44eb1c27d284522209353ba142f67
Aishwarya Indi <indiaishwarya97@gmail.com>
2025-11-02 01:33:09 +0000

    [LeetCode Sync] Runtime - 63 ms (12.65%), Memory - 54.8 MB (66.42%)

--- my-folder/0001-two-sum/solution.js
+++ my-folder/0001-two-sum/solution.js
@@ -6,9 +6,10 @@
 var twoSum = function(nums, target) {
     for(let i=0; i<nums.length; i++) {
         for(let j=1; j<nums.length; j++) {
-            if(i !== j && (nums[i] + nums[j] == target)) {
-                return [i,j]
+            if(nums[i]+nums[j] == target && i!==j) {
+                return [i,j];
             }
         }
     }
+    
 };
