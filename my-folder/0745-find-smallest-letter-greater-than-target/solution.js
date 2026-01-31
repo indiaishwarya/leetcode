86850e0fae2feeb95640fbf82408746ae7a63a99
Aishwarya Indi <indiaishwarya97@gmail.com>
2026-01-31 21:58:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 54.8 MB (72.55%)

---
+++ my-folder/0745-find-smallest-letter-greater-than-target/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    let ret = letters[0];
+    for(let i=0; i<letters.length; i++) {
+        if(letters[i] > target) {
+            ret = letters[i];
+            break;
+        }
+    }
+    return ret;
+};
